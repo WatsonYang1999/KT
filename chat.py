df490b7a413ede275d86e393dd6563c9f252cf15
Huasheng Yang <yanghs@amazon.com>
2023-08-23 20:36:54 +0800

    update

--- chat.py
+++ chat.py
@@ -1,15 +1,38 @@
 import os
 import openai
-import os
+
 openai.api_key = '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'
 
+# List of code snippets you want to label
+code_snippets = [
+  "for i in range(5):\n    print(i)",
+  "def add(a, b):\n    return a + b",
+  "class Circle:\n    def __init__(self, radius):\n        self.radius = radius"
+]
 
-completion = openai.ChatCompletion.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "user", "content": "Hello!"}
-  ]
-)
+# Set of programming knowledge points or skills
+knowledge_set = [
+  "loops",
+  "functions",
+  "classes"
+]
 
-print(completion.choices[0].message)
+# Initialize a dictionary to store the responses
+responses = {}
 
+# Loop through each code snippet and call the OpenAI API
+for idx, snippet in enumerate(code_snippets):
+  prompt = f"Label the following code snippet for its programming knowledge point: '{snippet}'\nKnowledge points: {', '.join(knowledge_set)}\nLabel:"
+
+  response = openai.Completion.create(
+    engine="text-davinci-003",  # You can also use "text-davinci-003" for shorter completions
+    prompt=prompt,
+    max_tokens=1  # Adjust the number of tokens according to your needs
+  )
+
+  label = response.choices[0].text.strip()
+  responses[snippet] = label
+
+# Print the responses
+for snippet, label in responses.items():
+  print(f"Code Snippet: {snippet}\nLabel: {label}\n")
---
+++ task_description.md
@@ -0,0 +1,17 @@
+## Call OpenAI API
+
+#### Tell ChatGPT to Write the script:
+
+##### Requirement
+
+I want to use open ai API to automate the task of labeling student programming code snippets.
+
+Write me a python scipt that call Open AI API and do the following loop:
+
+provides code snippets presented in a list, and a set of programming knowledege points or skill,
+
+let chatgpt label the code snippets in the set. 
+
+One code snippet require one call to the openAI api. And the knowledge set is fixed.
+
+Store the response content.
