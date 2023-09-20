752d2407563c08f0e822db700a64b045c1336202
WatsonYang1999 <watsonyang1999@163.com>
2023-09-20 16:22:49 +0800

    update commit message

--- localcodechange.bat
+++ localcodechange.bat
@@ -3,9 +3,20 @@
 REM Pull the latest changes from the remote repository
 git pull origin main
 
-REM Add and commit the changes
+REM Add the changes
 git add .
-git commit -m "update"
+
+REM Check if a commit message argument was provided
+IF "%~1"=="" (
+    REM If no commit message was provided, use a default message
+    set commit_message=update
+) ELSE (
+    REM If a commit message was provided, use it
+    set commit_message=%~1
+)
+
+REM Commit the changes with the specified message
+git commit -m "%commit_message%"
 
 REM Push the changes to the remote repository
 git push -u origin main
