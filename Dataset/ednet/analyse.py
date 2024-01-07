1cd0842c66ecf986571b0b010b899ba33a95a12b
WatsonYang1999 <watsonyang1999@163.com>
2024-01-07 22:12:14 +0800

    Remove cached dataset

--- Dataset/ednet/analyse.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-#import pandas as pd
-
-
-data = np.load('ednet.npz')
-
-y, skill, problem, real_len = data['y'], data['skill'], data['problem'], data['real_len']
-
-print(problem)
